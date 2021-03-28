7711a80600c44fa8f5c08abe29a45f7889936c8b
gabion10 <tcaciuc.ionut@gmail.com>
2021-03-28 22:10:59 +0300

    Created User Services and Blazor app

---
+++ UserService/Helpers/DataContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using UserService.Entities;
+
+namespace UserService.Helpers
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+        } 
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<User>()
+                .ToTable("User");
+
+        }
+    }
+}
